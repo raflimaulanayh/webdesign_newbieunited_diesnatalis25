47bf5a3f728b5cd0674c184dfcd2f84ee05865de
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2025-11-27 21:10:33 +0700

    fix: background color on title tentang section

--- src/components/organisms/tentang-kami/tentang/tentang.tsx
+++ src/components/organisms/tentang-kami/tentang/tentang.tsx
@@ -12,7 +12,7 @@ export const Tentang = () => {
           initial={{ scale: 0.8, opacity: 0 }}
           whileInView={{ scale: 1, opacity: 1 }}
           transition={{ duration: 0.5 }}
-          className="translate-y-1/2 -rotate-2 transform px-6 py-3 text-lg font-bold text-white md:px-10 md:text-3xl"
+          className="translate-y-1/2 -rotate-2 transform bg-secondary px-6 py-3 text-lg font-bold text-white md:px-10 md:text-3xl"
         >
           {ABOUT_DATA.badge.map((line, index) => (
             <span key={index} className="block text-center">
@@ -23,9 +23,9 @@ export const Tentang = () => {
       </div>
 
       <div className="relative z-10 rounded-t-xl bg-primary px-6 pt-24 pb-16 text-white md:rounded-t-4xl md:px-12 lg:px-[150px]">
-        <div className="p flex justify-center gap-12 bg-secondary md:gap-16 lg:flex-row lg:gap-24">
-          <div className="relative h-[300px] w-full rounded-sm bg-accent-orange px-0 md:rounded-2xl lg:h-[400px]">
-            <Image src={ABOUT_DATA.image} alt="Ilustrasi Food Waste" fill className="rounded-2xl" />
+        <div className="flex flex-col justify-center gap-12  md:gap-16 lg:flex-row lg:gap-24">
+          <div className="relative h-[300px] w-full rounded-sm md:rounded-2xl lg:h-[400px]">
+            <Image src={ABOUT_DATA.image} alt="Ilustrasi Food Waste" fill className="rounded-2xl object-cover" />
           </div>
 
           <motion.div
