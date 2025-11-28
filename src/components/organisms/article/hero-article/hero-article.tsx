d0fd8799f67ffe773cd4def7f726f4573553dd16
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2025-11-28 21:43:49 +0700

    fix: image and copywriting in article page

--- src/components/organisms/article/hero-article/hero-article.tsx
+++ src/components/organisms/article/hero-article/hero-article.tsx
@@ -5,7 +5,7 @@ import { Hero } from '@/components/templates/hero'
 export const HeroArticle = () => {
   return (
     <Hero
-      title="Jurnal Super Meal"
+      title="Level Up Your Lifestyle"
       description="Selami fakta mengejutkan tentang sampah makanan, inspirasi dari mitra lokal, dan langkah kecil yang bisa kita lakukan untuk bumi yang lebih hijau"
     />
   )
